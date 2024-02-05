811104a144b48bf66f2ec2e4b1af6836d9690805
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-02-05 13:40:45 +0900

    Update k8s_simple.py

--- dags/k8s_simple.py
+++ dags/k8s_simple.py
@@ -1,18 +1,52 @@
-# from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime, timedelta
 
-# from kubernetes.client import models as k8s
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
+from kubernetes.client import models as k8s
+from airflow.models import DAG, Variable
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.kubernetes.secret import Secret
+
+#from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+#    KubernetesPodOperator,
+#)
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+dag_id = 'kubernetes-dag'
+
+task_default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'execution_timeout': timedelta(hours=1)
+}
+
+dag = DAG(
+    dag_id=dag_id,
+    description='kubernetes pod operator',
+    default_args=task_default_args,
+    schedule_interval= None,
+    max_active_runs=1
+)
+    
+# Use k8s_client.V1ResourceRequirements to define resource limits
+k8s_resource_requirements = k8s.V1ResourceRequirements(
+    requests={"cpu": "0.2","memory": "100Mi"}, limits={"cpu": "0.5","memory": "512Mi"}
+)
+        
+start = DummyOperator(task_id="start", dag=dag)
+
+run = KubernetesPodOperator(
+    task_id="kubernetes-pod-operator",
+    namespace='default',
+    in_cluster=True,
+    image='nginx',
+    cluster_context='cluster_context',
+    #image='ghcr.io/rohminji/batch:master',
+    name="db-job",
+    is_delete_operator_pod=True,
+    get_logs=True,
+    service_account_name='edu',
+    # resources = k8s_resource_requirements,
+    dag=dag,
 )
 
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    do_xcom_push=True,
-)
-
-k.dry_run()
+start >> run
